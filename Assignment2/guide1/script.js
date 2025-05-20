2a843017f93ac2a369f61909a46f28d973a3ed2a
YikkwanSue <liyikkwan.18@gmail.com>
2025-05-20 19:00:15 +1000

    updated

--- Assignment2/guide1/script.js
+++ Assignment2/guide1/script.js
@@ -3,6 +3,9 @@ function playAudio() {
   }
   
   function goToIngredients() {
-    window.location.href = '../dough/index.html';
-  }
+    window.location.href = '../bake/index.html';
+}
+document.getElementById('bake-button').addEventListener('click', function () {
+    window.location.href = '../bake/index.html';
+  });
   
